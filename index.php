ccdd2a92f7e81ffd232434294a4d6125bb4a0519
Dawjaw <battlefront43@gmail.com>
2015-12-14 22:45:58 +0100

    change

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <!doctype html>
 <hmtl>
-	<head></head>
+	<head>
+	<title>Online-Shop</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	</head>
 	<header><h1>Online-Shop</h1><header>
 <body>
-<form>
-</form>
+	<form>
+		<div class="gl">
+		<label>Anmelden</label>
+		</div>
+		<div class="dl">
+		<label for="name">Name:</label>
+		<input type="text" name="name" id="name">
+		<label for="pw">Passwort:</label>
+		<input type="password" name="pw" id="pw">
+		</div>
+		<br>
+	</form>
+	<form>
+	</form>
 <?php
 
 
 ?>
-
 </body>
 <footer></footer>
 </html>
