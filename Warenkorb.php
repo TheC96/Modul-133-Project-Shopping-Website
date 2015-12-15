1a5b6c2e3b88da53a4b91ae73ca304c538be25e8
Dawjaw <battlefront43@gmail.com>
2015-12-15 10:12:38 +0100

    dsf

--- Warenkorb.php
+++ Warenkorb.php
@@ -6,20 +6,27 @@
 	if(isset($_POST['name'])){
 		$_SESSION['produkt'] = $_POST['name'];
 		$_SESSION['anzahl'] = $_POST['anzahl'];
+		$Preis = $_SESSION['anzahl'] * $_POST['preis'];
 
-	$_SESSION['waren'] = (isset($_SESSION['waren']))? $_SESSION['waren'] : array();
-	$_SESSION['waren'][$_SESSION['produkt']] = (isset($_SESSION['waren'][$_SESSION['produkt']]))?$_SESSION['waren'][$_SESSION['produkt']]: array(); 
-		$_SESSION['waren'][$_SESSION['produkt']][] = $_POST['name']. ": " .$_POST['anzahl'];
-		//echo implode('<br>', $_SESSION['waren'][$_SESSION['produkt']]);
-	}else if(isset($_SESSION['produkt'])) {
-		//echo implode('<br>', $_SESSION['waren'][$_SESSION['produkt']]);
+	$_SESSION['warenkorb'] = (isset($_SESSION['warenkorb']))? $_SESSION['warenkorb'] : array();
+	$_SESSION['warenkorb'][$_POST['name']] = (isset($_SESSION['warenkorb'][$_POST['name']]))?$_SESSION['warenkorb'][$_POST['name']]: array(); 
+		$_SESSION['warenkorb'][$_POST['name']][] = $_POST['name']. ": " .$_POST['anzahl']." stück". " für " .$Preis." CHF";
 	}
-	foreach ($_SESSION['waren'] as $key => $produkte){
-		//echo implode('<br>', $_SESSION['waren'][$_SESSION['produkt']]);
-		echo ($produkte);
+
+	
+
+	foreach ($_SESSION['warenkorb'] as $key => $produkte){	
+//var_dump($key);	
+			echo ('<br>'.$produkte[0]);
 	}
+	//[$_SESSION['produkt']]
+	//
+	//
 ?>
 </div>
+
+
+
 
 
 
