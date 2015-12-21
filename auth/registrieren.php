edb5b2ea9c955758012b0fed494e8c0b113a0bf2
TheC96 <steffen.carlos96@gmail.com>
2015-12-21 09:31:53 +0100

    update

--- auth/registrieren.php
+++ auth/registrieren.php
@@ -1,26 +1,37 @@
 <!doctype html>
 <hmtl>
 	<head>
-	<title>Online-Shop</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="../style.css">
+		<title>Online-Shop</title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="../style.css">
 	</head>
-	<header><h1>Online-Shop</h1><header>
-<body>
-	
-  <form action="register.php" method="post">
-  	<label for="user">Username</label>
-	<input type="text" name="user" id="user" required>
-  	<br>
-  	<label for="password">Password:</label>
-  	<input type="password" name="password" id="password" required><br>
-  	<label for="password2">Password Widerholen:</label>
-  	<input type="password" name="password2" id="password2" required>
-  	<br>
-  	<a href="loginform.php">Zur&uuml;ck zum Login</a><br>
-  	<input type="submit" value="register">
-  </form>
+	<body>
+		<header><h1>Online-Shop</h1><header>
+		<div class="aa">
+			<hr>
+		</div>
+		<ul>
+  			<li><a href="index.php">Waren</a></li>
+  			<li><a href="warenkorb.php">Warenkorb</a></li>
+		
+		</ul>	
+		<div id="seite">
+			<div id="inhalt">
 
-</body>
-<footer></footer>
+  			<form action="register.php" method="post">
+  				<label for="user">Username</label>
+				<input type="text" name="user" id="user" required>
+  				<br>
+			  	<label for="password">Password:</label>
+ 			 	<input type="password" name="password" id="password" required><br>
+ 			 	<label for="password2">Password Widerholen:</label>
+ 			 	<input type="password" name="password2" id="password2" required>
+			  	<br>
+ 			 	<a href="loginform.php">Zur&uuml;ck zum Login</a><br>
+ 			 	<input type="submit" value="register">
+ 			</form>
+			</div>
+			<footer id=fussbereich>&copy; 2015 Steffen & Persello</footer>
+		</div>
+	</body>
 </html>
